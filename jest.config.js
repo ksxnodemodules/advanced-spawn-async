68c281e1c37ad7b10ec59d3aa56b933acf7d5758
khai96_ <hvksmr1996@gmail.com>
2018-09-25 19:09:29 +0700

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '\\.test\\.tsx?',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ]
+}
